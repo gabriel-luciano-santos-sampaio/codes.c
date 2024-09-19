f4dcc1648680a785fb2bd782a16cf6217242dde3
Gabriel Luciano <gbriells983@gmail.com>
2024-09-19 18:52:38 -0300

    listas_encadeadas.c

---
+++ listas_encadeadas.c
@@ -0,0 +1,52 @@
+//listas encadeadas
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+typedef struct lista{
+    int dado;
+    struct lista*link;
+}no;
+
+no*crialista(int n){
+    no*ini, *p, *ult;
+    int valor;
+    ini=ult=NULL;
+    for(int i = 1; i<=n;i++){
+        printf("Digite o valor %d da lista: ", i);
+        scanf("%d",&valor);
+        p=(no*)malloc(sizeof(no));
+        p->dado=valor;
+        p->link=NULL;
+        if(ult!=NULL)
+            ult->link=p;
+        else
+            ini=p;
+        ult=p;
+        
+return ini;
+    }
+}
+void escrevelista(no*p){
+    while (p!=NULL){
+        printf("%d\t", p->dado);
+        p=p->link;
+    }
+}
+int main(){
+    no*primeira;
+    int n;
+    printf("Criando uma lista encadeada");
+    do{
+        printf("\nEntre com o numero de nois: ");
+        scanf ("%d", &n);
+        }while(n<0);
+    primeira = crialista(n);
+    if(primeira != NULL){
+        printf("\n Lista criada");
+        printf("\n");
+        escrevelista(primeira);
+    }
+    else
+        printf("\nLista vazia");
+    return(0);
+}
